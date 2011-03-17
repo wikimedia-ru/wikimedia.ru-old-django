b99ec765a3e5c1286c5547a357cb824de4f87888
Sergey Leschina <i.am.putnik@gmail.com>
2011-03-18 02:36:16 +0300

    Динамические пути к загружаемым файлам в зависимости от страницы.

--- pages/admin.py
+++ pages/admin.py
@@ -2,12 +2,11 @@ from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
 
 from pages.models import Page, PageFile
-#from files.models import File
 
 
 
 class PageFileInline(admin.TabularInline):
-        model = PageFile
+    model = PageFile
 
 
 class PageAdmin(admin.ModelAdmin):
@@ -15,9 +14,6 @@ class PageAdmin(admin.ModelAdmin):
         (None, {
             'fields': ('title', 'extend_title', 'section', 'url', 'old_url', 'active', 'text')
         }),
-        #(_('Attached files'), {
-        #    'fields': ()
-        #}),
     )
     inlines = (PageFileInline,)
 
--- pages/models.py
+++ pages/models.py
@@ -1,5 +1,3 @@
-import os
-
 from django.utils.translation import ugettext_lazy as _
 from django.db import models
 from django.conf import settings
@@ -20,7 +18,12 @@ class Page(models.Model):
 
 
 class PageFile(models.Model):
+    def make_upload_path(instance, filename):
+        if instance.page.section:
+            return u"%s/%s/%s/%s" % (settings.MEDIA_ROOT, instance.page.section.url, instance.page.url, filename)
+        else:
+            return u"%s/%s/%s" % (settings.MEDIA_ROOT, instance.page.url, filename)
+    
     page = models.ForeignKey('Page', verbose_name=_("Page"))
-    file = models.FileField(upload_to=settings.MEDIA_ROOT, blank=True, null=True, verbose_name=_("File"))
-    #file = models.FileField(upload_to=os.path.join(settings.MEDIA_ROOT, page.url), blank=True, null=True, verbose_name=_("File"))
+    file = models.FileField(upload_to=make_upload_path, blank=True, null=True, verbose_name=_("File"))
 
