817287d949309ea1bbef2a6f80eecc3d3c5b7f9b
Sergey Leschina <i.am.putnik@gmail.com>
2011-03-19 01:55:55 +0300

    Поля телефона и суммы вместо простого значения строятся по шаблону.

--- donate/views.py
+++ donate/views.py
@@ -1,3 +1,5 @@
+import re
+
 from django.template.loader import render_to_string
 from django.template import RequestContext
 
@@ -7,6 +9,11 @@ from donate.models import Donation
 
 def donations_list(request):
     d_list = Donation.objects.order_by('order')
+    for d_item in d_list:
+        if d_item.phone_id.find('='):
+            d_item.phone_tpl = '<span class="d-tpl">' + re.sub(r'\[(?P<id>[^\[\]=]+)=(?P<len>[^\[\]=]+)\]', '<input type="text" name="\g<id>" maxlength="\g<len>" size="\g<len>" />', d_item.phone_id) + '</span>'
+        if d_item.amount_id.find('='):
+            d_item.amount_tpl = '<span class="d-tpl">' + re.sub(r'\[(?P<id>[^\[\]=]+)=(?P<len>[^\[\]=]+)\]', '<input type="text" name="\g<id>" maxlength="\g<len>" size="\g<len>" />', d_item.amount_id) + '</span>'
     
     return render_to_string('donate/donations_list.html', {
         'donations': d_list,
