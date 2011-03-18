f4d3c4ba89d69ea44c97bb1f5d2e5491c3ebea6d
Sergey Leschina <i.am.putnik@gmail.com>
2011-03-19 02:01:47 +0300

    Мелкое исправление обработки составных полей телефона и суммы.

--- donate/views.py
+++ donate/views.py
@@ -10,9 +10,9 @@ from donate.models import Donation
 def donations_list(request):
     d_list = Donation.objects.order_by('order')
     for d_item in d_list:
-        if d_item.phone_id.find('='):
+        if d_item.phone_id.find('=') >= 0:
             d_item.phone_tpl = '<span class="d-tpl">' + re.sub(r'\[(?P<id>[^\[\]=]+)=(?P<len>[^\[\]=]+)\]', '<input type="text" name="\g<id>" maxlength="\g<len>" size="\g<len>" />', d_item.phone_id) + '</span>'
-        if d_item.amount_id.find('='):
+        if d_item.amount_id.find('=') >= 0:
             d_item.amount_tpl = '<span class="d-tpl">' + re.sub(r'\[(?P<id>[^\[\]=]+)=(?P<len>[^\[\]=]+)\]', '<input type="text" name="\g<id>" maxlength="\g<len>" size="\g<len>" />', d_item.amount_id) + '</span>'
     
     return render_to_string('donate/donations_list.html', {
